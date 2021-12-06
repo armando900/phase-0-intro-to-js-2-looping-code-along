7d652fa231cf31c8ae9534e88e21c0bd4ec51a48
Armando <seniorbuso@gmail.com>
2021-12-06 12:54:35 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const action = 'surprise';
+
+function writeCards(names) {
+    let newArr = [];
+    for (let x=0; x < names.length; x++) {
+        newArr.push(`Thank you, ${names[x]}, for the wonderful ${action} gift!`);
+        debugger;
+    }
+    return newArr;
+}
+writeCards(names);
+
+function countDown(numb) {
+
+    while (numb >= 0) {
+        console.log(numb--);
+    }
+
+}
